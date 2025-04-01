2538785dc4912f3f75aed34873613a34d89ba89e
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-04-01 12:23:00 +0500

    Update models.py
    Отформатировал

--- places/models.py
+++ places/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+
 from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=150, unique=True, null=False, default="",
-                             blank=False, verbose_name="Название места")
+    title = models.CharField(
+        max_length=150, unique=True, null=False,
+        blank=False, verbose_name="Название места"
+    )
+
     short_description = models.TextField(
-        verbose_name="Короткое описание места", null=False, default="", blank=True)
+        verbose_name="Короткое описание места", null=False,
+        default="", blank=True
+    )
     long_description = HTMLField(
-        verbose_name="Подробное описание места", null=False, default="", blank=True)
+        verbose_name="Подробное описание места", null=False,
+        default="", blank=True
+    )
     longitude = models.FloatField(verbose_name="Долгота")
     latitude = models.FloatField(verbose_name="Широта")
 
@@ -18,10 +26,14 @@ class Place(models.Model):
 
 class Image(models.Model):
     place = models.ForeignKey(
-        Place, on_delete=models.CASCADE, related_name='images', verbose_name="Картинки")
+        Place, on_delete=models.CASCADE, related_name='images',
+        verbose_name="Картинки"
+    )
     image = models.ImageField(upload_to='images/', verbose_name="Картинка")
     order = models.PositiveIntegerField(
-        default=0, blank=False, null=False, verbose_name="порядок", db_index=True)
+        default=0, blank=False, null=False,
+        verbose_name="порядок", db_index=True
+    )
 
     class Meta:
         ordering = ['order']
